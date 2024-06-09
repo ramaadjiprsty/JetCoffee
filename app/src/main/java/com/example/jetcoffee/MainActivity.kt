561d3b631fcbbe5d715e4774f6adfd2ecbbd9010
Enneva <ramaadjiprsty@gmail.com>
2024-06-09 10:48:44 +0700

    creating menu list

--- app/src/main/java/com/example/jetcoffee/MainActivity.kt
+++ app/src/main/java/com/example/jetcoffee/MainActivity.kt
@@ -14,6 +14,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,8 +26,12 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.jetcoffee.model.Menu
+import com.example.jetcoffee.model.dummyBestSellerMenu
 import com.example.jetcoffee.model.dummyCategory
+import com.example.jetcoffee.model.dummyMenu
 import com.example.jetcoffee.ui.components.CategoryItem
+import com.example.jetcoffee.ui.components.MenuItem
 import com.example.jetcoffee.ui.components.Search
 import com.example.jetcoffee.ui.components.SectionText
 import com.example.jetcoffee.ui.theme.JetCoffeeTheme
@@ -33,7 +39,6 @@ import com.example.jetcoffee.ui.theme.JetCoffeeTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             JetCoffeeTheme {
                 JetCoffeeTheme {
@@ -46,10 +51,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun JetCoffeeApp() {
-    Column {
+    Column(modifier = Modifier
+        .verticalScroll(rememberScrollState())) {
         Banner()
         SectionText(stringResource(id = R.string.section_category))
         CategoryRow()
+        SectionText(stringResource(id = R.string.section_favorite_menu))
+        MenuRow(listMenu = dummyMenu)
+        SectionText(stringResource(id = R.string.section_best_seller_menu))
+        MenuRow(
+            listMenu = dummyBestSellerMenu,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
     }
 }
 
@@ -72,12 +85,29 @@ fun CategoryRow(
     LazyRow(
         horizontalArrangement = Arrangement.spacedBy(8.dp),
         contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
     ) {
         items(dummyCategory, key = {it.textCategory}) { category ->
             CategoryItem(category)
         }
     }
     
+}
+
+@Composable
+fun MenuRow(
+    listMenu: List<Menu>,
+    modifier: Modifier = Modifier
+) {
+   LazyRow(
+       horizontalArrangement = Arrangement.spacedBy(16.dp),
+       contentPadding = PaddingValues(horizontal = 16.dp),
+       modifier = modifier
+   ) {
+       items(listMenu, key = {it.title}) { menu ->
+           MenuItem(menu = menu)
+       }
+   }
 }
 
 @Preview(showBackground = true, device = Devices.PIXEL_4)
